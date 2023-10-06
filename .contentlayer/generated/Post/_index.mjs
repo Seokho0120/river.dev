7abc041327f423a78aff776954d3297f5b962a35
Seokho <seokho0120@gmail.com>
2023-10-06 14:53:54 +0900

    add: meta-tag.mdx 추가

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -4,5 +4,6 @@ import firstMdx from './first.mdx.json' assert { type: 'json' }
 import secondMdx from './second.mdx.json' assert { type: 'json' }
 import testMdx from './test.mdx.json' assert { type: 'json' }
 import thirdMdx from './third.mdx.json' assert { type: 'json' }
+import metaTagMdx from './meta-tag.mdx.json' assert { type: 'json' }
 
-export const allPosts = [firstMdx, secondMdx, testMdx, thirdMdx]
+export const allPosts = [firstMdx, secondMdx, testMdx, thirdMdx, metaTagMdx]
