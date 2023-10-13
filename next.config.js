1b4046fe81b11d6daa639c54a1f0b4342a82080f
Seokho <seokho0120@gmail.com>
2023-10-13 19:26:34 +0900

    fix: next.config 수정, 1편 수정

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 const { withContentlayer } = require('next-contentlayer');
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  formats: ['image/*'],
-};
+const nextConfig = {};
 
 module.exports = withContentlayer(nextConfig);
