6f9e092aa000544964ca0147059978275099261a
Seokho <seokho0120@gmail.com>
2023-10-17 22:41:11 +0900

    fix: layout 코드 원복

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import Script from 'next/script';
 import { Open_Sans } from 'next/font/google';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Provider from 'src/components/Provider';
 import { AnalyticsContainer } from '../components/analytics';
-import * as gtag from '../libs/gtags';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 const sans = Open_Sans({ subsets: ['latin'] });
 
@@ -49,45 +45,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    router.events.on('hashChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-      router.events.off('hashChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <html lang='ko' className={sans.className}>
       <meta
         name='google-site-verification'
         content='DrXuqvDuhqEVVfKJMO_1BwzoHwMQ7jktZxGV1XOB56c'
-      />
-
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy='afterInteractive'
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
-      />
-      <Script
-        id='gtag-init'
-        strategy='afterInteractive'
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
       />
 
       <body className='w-full flex flex-col overflow-auto antialiased max-w-screen-md mx-auto px-4 dark:bg-gray-900'>
