2cdd93ee0757d3c838fd7bdf23621184ad0477bd
Seokho <seokho0120@gmail.com>
2023-10-17 21:34:15 +0900

    fix: Script - id, strategy 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,8 +56,11 @@ export default function RootLayout({
       <Script
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy='afterInteractive'
       />
-      <script
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
         dangerouslySetInnerHTML={{
           __html: `
           window.dataLayer = window.dataLayer || [];
