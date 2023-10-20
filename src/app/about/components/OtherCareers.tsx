46ebb73b6b3f052891e829c3b866604f5ca6cd24
Seokho <seokho0120@gmail.com>
2023-10-20 19:56:24 +0900

    add: Other Careers 완성

--- src/app/about/components/OtherCareers.tsx
+++ src/app/about/components/OtherCareers.tsx
@@ -1,9 +1,39 @@
+import { OTHERCAREERS_MENU } from '@/src/constants/OtherCareersData';
+import Link from 'next/link';
+
 export default function OtherCareers() {
   return (
     <section className='mb-10'>
       <h1 className='text-4xl font-bold mb-4 underline underline-offset-2 decoration-wavy decoration-point dark:decoration-orange-400 decoration-2'>
         OtherCareers.
       </h1>
+
+      {OTHERCAREERS_MENU.map(({ title, job, date, lists, href }, idx) => (
+        <div className='flex mb-6' key={idx}>
+          <div className='w-1/3'>
+            <p className='text-xl font-bold mb-1 cursor-pointer hover:text-point dark:hover:text-darkPoint'>
+              <Link href={href}>{title}</Link>
+            </p>
+            <p className='text-sm italic text-neutral-600 dark:text-neutral-400'>
+              {job}
+            </p>
+            <p className='text-sm italic text-neutral-600 dark:text-neutral-400'>
+              {date}
+            </p>
+          </div>
+          <ul className='w-2/3'>
+            {lists.map((list, idx) => (
+              <li
+                key={idx}
+                className='relative flex items-start gap-2 mb-1 pl-3 whitespace-pre-line'
+              >
+                <circle className='w-1 h-1 bg-point dark:bg-darkPoint rounded-full absolute left-0 top-[0.6rem] inline-block' />
+                {list}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </section>
   );
 }
---
+++ src/constants/OtherCareersData.ts
@@ -0,0 +1,24 @@
+export const OTHERCAREERS_MENU = [
+  {
+    title: '현대백화점',
+    job: 'VMD 디자이너 & 팀장',
+    date: '2018.03 - 2021.11',
+    href: 'https://wecode.co.kr/',
+    lists: [
+      '입사 3년차에 실력을 인정받아 신촌점 VMD 팀장으로 승진',
+      '정량적 데이터를 기반으로 2021년 전년대비 30%이상 판매율 상승에 기여',
+      '다양한 직군과의 협업으로 유연한 커뮤니케이션 역량 습득',
+    ],
+  },
+  {
+    title: '915 Studio',
+    job: '디자이너 & CEO',
+    date: '2015.09 - 2017.05',
+    href: 'https://www.yoonbility.com/lecture.php?action=view&no=34&date_no=721',
+    lists: [
+      '창업 1년만에 2016 서울디자인페스벌 영 디자이너 선정',
+      '카카오메이커스 등 유명 리빙 편집샵 입정 및 96% 판매율 달성',
+      '다수의 디자이너들과 협업하여 전시 참여 및 사업지원 프로그램 선정',
+    ],
+  },
+];
