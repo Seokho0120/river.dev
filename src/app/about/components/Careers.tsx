109632a7ee2e903647db4ce2ebc13f4a2fc3eb8d
Seokho <seokho0120@gmail.com>
2023-10-20 15:53:42 +0900

    fix: hover 링크 수정

--- src/app/about/components/Careers.tsx
+++ src/app/about/components/Careers.tsx
@@ -39,7 +39,7 @@ export default function Careers() {
       {CARRERS_MENU.map(({ title, job, date, lists, href }, idx) => (
         <div className='flex mb-6' key={idx}>
           <div className='w-1/3'>
-            <p className='text-xl font-bold mb-1 cursor-pointer hover:underline decoration-point dark:decoration-darkPoint underline-offset-2'>
+            <p className='text-xl font-bold mb-1 cursor-pointer underline  hover:no-underline  decoration-point dark:decoration-darkPoint underline-offset-4'>
               <Link href={href}>{title}</Link>
             </p>
             <p className='text-sm italic text-neutral-600 dark:text-neutral-400'>
