054e63b1aa782db857e50fac2f840f11324e2f6a
Seokho <seokho0120@gmail.com>
2023-10-20 21:46:35 +0900

    add: Education, Other Careers 링크 추가

--- src/app/about/components/Education.tsx
+++ src/app/about/components/Education.tsx
@@ -12,7 +12,9 @@ export default function Education() {
         <div className='flex mb-6' key={idx}>
           <div className='w-1/3'>
             <p className='text-xl font-bold mb-1 cursor-pointer hover:text-point dark:hover:text-darkPoint'>
-              <Link href={href}>{title}</Link>
+              <Link href={href} target='_blank' rel='noopener noreferrer'>
+                {title}
+              </Link>
             </p>
             <p className='text-sm italic text-neutral-600 dark:text-neutral-400'>
               {job}
--- src/constants/OtherCareersData.ts
+++ src/constants/OtherCareersData.ts
@@ -3,7 +3,7 @@ export const OTHERCAREERS_MENU = [
     title: '현대백화점',
     job: 'VMD 디자이너 & 팀장',
     date: '2018.03 - 2021.11',
-    href: 'https://wecode.co.kr/',
+    href: 'https://auspicious-move-42f.notion.site/Visual-Merchandiser-51041ff014ff41988bb2264410876bfe?pvs=4',
     lists: [
       '입사 3년차에 실력을 인정받아 신촌점 VMD 팀장으로 승진',
       '정량적 데이터를 기반으로 2021년 전년대비 30%이상 판매율 상승에 기여',
@@ -14,7 +14,7 @@ export const OTHERCAREERS_MENU = [
     title: '915 Studio',
     job: '디자이너 & CEO',
     date: '2015.09 - 2017.05',
-    href: 'https://www.yoonbility.com/lecture.php?action=view&no=34&date_no=721',
+    href: 'https://auspicious-move-42f.notion.site/915-Studio-Product-Designer-93394f0e600d4b2d99e30ec376d45d48?pvs=4',
     lists: [
       '창업 1년만에 2016 서울디자인페스벌 영 디자이너 선정',
       '카카오메이커스 등 유명 리빙 편집샵 입정 및 96% 판매율 달성',
