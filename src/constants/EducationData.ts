1d502bcaa860af975fbe27c78488be270ba83d78
Seokho <seokho0120@gmail.com>
2023-10-20 18:24:19 +0900

    add: Education 완성

---
+++ src/constants/EducationData.ts
@@ -0,0 +1,36 @@
+export const EDUCATION_MENU = [
+  {
+    title: 'Wecode',
+    job: '프론트엔드 28기 수료',
+    date: '2021.11 - 2022.02',
+    href: 'https://wecode.co.kr/',
+    lists: [
+      `비교적 늦은 나이에 개발을 시작한만큼, 혼자 공부할때 생기는 삽질의 과정과 시간 낭비를 최대한 줄이고자 직접 멘토들과 오프라인으로 대면할 수 있는 
+      위코드 부트캠프 선택`,
+      '현업자 코드 리뷰, 팀 프로젝트, 서비스 운영 및 유지보수 경험',
+      '일정 주기 스프린트 개발을 통해 애자일 프로세스 경험',
+    ],
+  },
+  {
+    title: '윤빌리티',
+    job: 'UI/UX 포트폴리오 완성반 수료',
+    date: '2020',
+    href: 'https://www.yoonbility.com/lecture.php?action=view&no=34&date_no=721',
+    lists: [
+      '사용자 분석부터 서비스 기획, 화면 설계 및 디자인까지 학습',
+      'Grid System 기본 개념 학습(디바이스, 반응형 등)',
+      'UI Design System 구현',
+    ],
+  },
+  {
+    title: '리메인',
+    job: 'UI/UX 프로패셔널 14기 수료',
+    date: '2020',
+    href: 'https://remain.co.kr/shop/item_class.php?it_id=1647409970',
+    lists: [
+      'UX에 관심이 생겨 강의 참여',
+      'Wire Frame 구조 및 UI 디자인 구현',
+      'Prototyping & UT(Usability Test) 실전 경험',
+    ],
+  },
+];
