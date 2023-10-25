02f223a9193f1681f72e3226fe980a61528ee12d
Seokho <seokho0120@gmail.com>
2023-10-25 18:04:22 +0900

    add: FnF 내용 추가, 링크 추가

--- src/app/about/components/FnF.tsx
+++ src/app/about/components/FnF.tsx
@@ -15,6 +15,8 @@ export default function FnF() {
           video_href,
           github,
           github_href,
+          review,
+          review_href,
         }) => (
           <div key={href} className='flex flex-col my-4'>
             <div className='mb-2'>
@@ -47,6 +49,16 @@ export default function FnF() {
                   aria-label={video_href}
                 >
                   {video}
+                </Link>{' '}
+                |{' '}
+                <Link
+                  href={review_href}
+                  className='text-point dark:text-darkPoint underline underline-offset-2 decoration-point dark:decoration-darkPoint hover:no-underline'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  aria-label={review_href}
+                >
+                  {review}
                 </Link>
               </p>
             </div>
@@ -59,16 +71,40 @@ export default function FnF() {
         )
       )}
 
-      {FNF_WORKS.map(({ title, contents }, idx) => (
+      {FNF_WORKS.map(({ title, contents, title_href, sub_href }, idx) => (
         <div key={idx}>
-          <h2 className='font-bold text-xl'>{title}</h2>
+          <h2 className='flex items-center font-bold text-xl'>
+            {title}
+            {title_href && (
+              <Link
+                className='text-sm ml-1 text-point dark:text-darkPoint underline decoration-point dark:decoration-darkPoint hover:no-underline'
+                href={title_href}
+                target='_blank'
+                rel='noopener noreferrer'
+                aria-label={title_href}
+              >
+                [상세보기]
+              </Link>
+            )}
+          </h2>
           {contents.map((content, idx) => (
             <li
               key={idx}
-              className='font-medium relative flex items-start gap-2 mb-1 pl-7 whitespace-pre-line'
+              className='font-medium relative flex items-center mb-1 pl-7 whitespace-pre-line'
             >
               <div className='w-1 h-1 bg-point dark:bg-darkPoint rounded-full absolute left-4 top-[0.6rem] inline-block' />
               {content}
+              {sub_href && content === '재사용 컴포넌트 관리의 중요성 경험' && (
+                <Link
+                  className='ml-1 font-bold text-sm text-point dark:text-darkPoint underline decoration-point dark:decoration-darkPoint hover:no-underline'
+                  href={sub_href}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  aria-label={sub_href}
+                >
+                  [상세보기]
+                </Link>
+              )}
             </li>
           ))}
         </div>
--- src/constants/FnF.ts
+++ src/constants/FnF.ts
@@ -4,11 +4,15 @@ export const FNF_INFO = [
     date: '2022.01 - 2022.02',
     href: 'https://www.youtube.com/watch?v=A85Y6BtZaqw',
     skills: 'Skills',
-    skill_list: 'React, TypeScript, Styled-component, Recoil, Recharts, MUI',
+    skill_list:
+      'React, TypeScript, Styled-component, Recoil, Rechart.js, Material-ui',
     video: '시연영상',
     video_href: 'https://www.youtube.com/watch?v=A85Y6BtZaqw',
     github: 'Github',
     github_href: 'https://github.com/Seokho0120/wecode-ojt-fnf-frontend',
+    review: '회고',
+    review_href:
+      'https://velog.io/@leesegho/%EC%97%90%ED%94%84%EC%95%A4%EC%97%90%ED%94%84-%ED%9B%84%EA%B8%B0',
   },
 ];
 
@@ -23,13 +27,18 @@ export const FNF_WORKS = [
   },
   {
     title: '재사용 가능한 컴포넌트 개발',
+    sub_href:
+      'https://velog.io/@leesegho/FF-Components-%EC%9E%AC%EC%82%AC%EC%9A%A9%EC%97%90-%EB%8C%80%ED%95%9C-%EA%B3%A0%EB%AF%BC%EA%B3%BC-%EA%B2%BD%ED%97%98',
     contents: [
       '화면마다 무분별하게 중복되는 컴포넌트 이슈 → 재사용성을 극대화하기 위해 공통 컴포넌트를 개발',
       '짧은 기간안에 빠르게 차트와 테이블을 구현할 수 있는 Recharts와 MUI 라이브러리를 활용하여 화면마다 필요한 스타일 커스텀 및 공유',
+      '재사용 컴포넌트 관리의 중요성 경험',
     ],
   },
   {
     title: 'UI/UX 디자인 및 기획',
     contents: ['UI/UX 디자인과 기획 리딩, 스타일 가이드 공유'],
+    title_href:
+      'https://www.notion.so/STYLE-GUIDE-901c8c2fe15c4665a6cc5e8821465239',
   },
 ];
