309a17f5eb74b3e3d47fddcefc4c62af8a996030
Seokho <seokho0120@gmail.com>
2023-10-27 17:45:56 +0900

    fix: 블로그 내용 수정

--- src/constants/RiverBlog.ts
+++ src/constants/RiverBlog.ts
@@ -31,8 +31,8 @@ export const RiverBlog_WORKS = [
     title: 'SEO 최적화',
     href: 'https://river-dev.vercel.app/posts/create-blog-3#nextjs%EC%9D%98-seo%EC%B5%9C%EC%A0%81%ED%99%94',
     contents: [
-      `정적 페이지는 metadata객체를 활용해 metadata를 생성하고, 
-      동적 라우팅 페이지는 generateMetadata를 활용해 동적으로 metadata를 생성`,
+      `정적 페이지는 metadata객체를 활용해 Metadata를 생성하고, 
+      동적 라우팅 페이지는 generateMetadata 함수를 활용해 동적으로 Metadata를 생성`,
       'Metadata Files API를 활용해 sitemap.xml 파일과 robots.txt를 추가하여 검색 엔진 최적화',
       'Vercel Analytics, Google Analytics를 연동해 다채로운 통계데이터 확인',
       '검색 시 노출도를 상승 시키기 위해 Google Search Console에 사이트를 등록',
@@ -49,9 +49,7 @@ export const RiverBlog_WORKS = [
   {
     title: '다크모드 구현',
     href: 'https://river-dev.vercel.app/posts/create-blog-3#%EB%8B%A4%ED%81%AC%EB%AA%A8%EB%93%9C',
-    contents: [
-      'Next.js 13환경에서 최고의 효율을 보여주는 next-themes와 tawilwind 조합으로 다크모드 개발',
-    ],
+    contents: ['next-themes와 tawilwind 조합으로 다크모드 개발'],
   },
   {
     title: '캐러셀 구현',
